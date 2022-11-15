ce2d542a06a0a380d124bc613f94a8814dc5bcd7
SL313 <rebin0313@gmail.com>
2022-11-15 09:01:22 +0900

    메인 페이지 디자인

--- lib/screen/custom_screen/custom_screen.dart
+++ lib/screen/custom_screen/custom_screen.dart
@@ -19,9 +19,9 @@ class _CustomizeScreenState extends State<CustomizeScreen> {
 
   @override
   void initState() {
-    // TODO: implement initState
     Future.delayed(Duration(seconds: 0)).then((value) =>
         Provider.of<StoreTheme>(context, listen: false).clearTheme());
+    print(Provider.of<StoreTheme>(context, listen: false).country);
     super.initState();
   }
 
@@ -29,42 +29,46 @@ class _CustomizeScreenState extends State<CustomizeScreen> {
     return Scaffold(
       appBar: context.watch<Store>().index == -1
           ? AppBar(
-              backgroundColor: Colors.black87,
-              actions: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
-                  child: IconButton(
-                      onPressed: () {
-                        StoreTheme theme = StoreTheme();
-                        theme.country =
-                            Provider.of<StoreTheme>(context, listen: false)
-                                .country;
-                        theme.backgroundTheme =
-                            Provider.of<StoreTheme>(context, listen: false)
-                                .backgroundTheme;
-                        theme.clockTheme =
-                            Provider.of<StoreTheme>(context, listen: false)
-                                .clockTheme;
-                        theme.clockColor =
-                            Provider.of<StoreTheme>(context, listen: false)
-                                .clockColor;
-                        theme.textColor =
-                            Provider.of<StoreTheme>(context, listen: false)
-                                .textColor;
-                        Provider.of<Store>(context, listen: false)
-                            .getTheme(theme);
-                        Navigator.pop(context);
-                      },
-                      icon: Icon(
-                        Icons.add,
-                        size: 28,
-                      )),
-                )
-              ],
-            )
-          : AppBar(
-              backgroundColor: Colors.black87,
+        backgroundColor: Colors.black87,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
+            child: TextButton(
+              onPressed: () {
+                StoreTheme theme = StoreTheme();
+                theme.country =
+                    Provider.of<StoreTheme>(context, listen: false)
+                        .country;
+                theme.backgroundTheme =
+                    Provider.of<StoreTheme>(context, listen: false)
+                        .backgroundTheme;
+                theme.clockTheme =
+                    Provider.of<StoreTheme>(context, listen: false)
+                        .clockTheme;
+                theme.clockColor =
+                    Provider.of<StoreTheme>(context, listen: false)
+                        .clockColor;
+                theme.textColor =
+                    Provider.of<StoreTheme>(context, listen: false)
+                        .textColor;
+                Provider.of<Store>(context, listen: false)
+                    .getTheme(theme);
+                Navigator.pop(context);
+              },
+              child: const Text('ADD',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontFamily: 'main',
+                ),
+              ),
             ),
+          )
+        ],
+      )
+          : AppBar(
+        backgroundColor: Colors.black87,
+      ),
       body: Container(
         child: Stack(
           children: [
@@ -108,8 +112,8 @@ class _CustomizeScreenState extends State<CustomizeScreen> {
           ],
           showUnselectedLabels: true,
           currentIndex: _selectedIndex,
-          selectedItemColor: Colors.lightBlue,
-          unselectedItemColor: Colors.orange,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.grey,
           onTap: (int value) {
             setState(() {
               if (_currentIndex == value) {
@@ -151,9 +155,9 @@ class _ThemeColorState extends State<ThemeColor> with TickerProviderStateMixin {
                     decoration: BoxDecoration(
                         border: Border(
                             right: BorderSide(
-                      color: Colors.black12,
-                      width: 4,
-                    ))),
+                              color: Colors.black12,
+                              width: 4,
+                            ))),
                     child: RotatedBox(
                       quarterTurns: 1,
                       child: Align(
@@ -193,46 +197,46 @@ class _ThemeColorState extends State<ThemeColor> with TickerProviderStateMixin {
                           pickerColor: context.watch<Store>().index == -1
                               ? context.watch<StoreTheme>().textColor
                               : context
-                                  .watch<Store>()
-                                  .storedThemes[context.watch<Store>().index]
-                                  .textColor,
+                              .watch<Store>()
+                              .storedThemes[context.watch<Store>().index]
+                              .textColor,
                           enableAlpha: false,
                           colorModel: ColorModel.rgb,
                           indicatorSize: const Size(200, 15),
                           onColorChanged: (color) {
                             Provider.of<Store>(context, listen: false).index ==
-                                    -1
+                                -1
                                 ? Provider.of<StoreTheme>(context,
-                                        listen: false)
-                                    .setTextColor(color)
+                                listen: false)
+                                .setTextColor(color)
                                 : Provider.of<Store>(context, listen: false)
-                                    .storedThemes[Provider.of<Store>(context,
-                                            listen: false)
-                                        .index]
-                                    .setTextColor(color);
+                                .storedThemes[Provider.of<Store>(context,
+                                listen: false)
+                                .index]
+                                .setTextColor(color);
                           },
                         ),
                         SlidePicker(
                           pickerColor: context.watch<Store>().index == -1
                               ? context.watch<StoreTheme>().clockColor
                               : context
-                                  .watch<Store>()
-                                  .storedThemes[context.watch<Store>().index]
-                                  .clockColor,
+                              .watch<Store>()
+                              .storedThemes[context.watch<Store>().index]
+                              .clockColor,
                           enableAlpha: false,
                           colorModel: ColorModel.rgb,
                           indicatorSize: const Size(200, 15),
                           onColorChanged: (color) {
                             Provider.of<Store>(context, listen: false).index ==
-                                    -1
+                                -1
                                 ? Provider.of<StoreTheme>(context,
-                                        listen: false)
-                                    .setClockColor(color)
+                                listen: false)
+                                .setClockColor(color)
                                 : Provider.of<Store>(context, listen: false)
-                                    .storedThemes[Provider.of<Store>(context,
-                                            listen: false)
-                                        .index]
-                                    .setClockColor(color);
+                                .storedThemes[Provider.of<Store>(context,
+                                listen: false)
+                                .index]
+                                .setClockColor(color);
                           },
                         ),
                       ],
@@ -261,24 +265,24 @@ class Background extends StatelessWidget {
           scrollDirection: Axis.horizontal,
           child: Row(
               children: List.generate(10, (index) {
-            return GestureDetector(
-              key: ValueKey(index),
-              onTap: () {
-                Provider.of<Store>(context, listen: false).index == -1
-                    ? Provider.of<StoreTheme>(context, listen: false)
+                return GestureDetector(
+                  key: ValueKey(index),
+                  onTap: () {
+                    Provider.of<Store>(context, listen: false).index == -1
+                        ? Provider.of<StoreTheme>(context, listen: false)
                         .setBackground(index)
-                    : Provider.of<Store>(context, listen: false)
+                        : Provider.of<Store>(context, listen: false)
                         .storedThemes[
-                            Provider.of<Store>(context, listen: false).index]
+                    Provider.of<Store>(context, listen: false).index]
                         .setBackground(index);
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: Image.asset('assets/background/small/small_bg$index.jpg',
-                    fit: BoxFit.contain),
-              ),
-            );
-          })),
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Image.asset('assets/background/small/small_bg$index.jpg',
+                        fit: BoxFit.contain),
+                  ),
+                );
+              })),
         ),
       ),
     );
@@ -302,26 +306,26 @@ class Clock extends StatelessWidget {
           scrollDirection: Axis.horizontal,
           child: Row(
               children: List.generate(5, (index) {
-            return GestureDetector(
-              key: ValueKey(index),
-              onTap: () {
-                Provider.of<Store>(context, listen: false).index == -1
-                    ? Provider.of<StoreTheme>(context, listen: false)
+                return GestureDetector(
+                  key: ValueKey(index),
+                  onTap: () {
+                    Provider.of<Store>(context, listen: false).index == -1
+                        ? Provider.of<StoreTheme>(context, listen: false)
                         .setClock(index)
-                    : Provider.of<Store>(context, listen: false)
+                        : Provider.of<Store>(context, listen: false)
                         .storedThemes[
-                            Provider.of<Store>(context, listen: false).index]
+                    Provider.of<Store>(context, listen: false).index]
                         .setClock(index);
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image.asset(
-                  'assets/clock_layout/clock$index.png',
-                  fit: BoxFit.contain,
-                ),
-              ),
-            );
-          })),
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(
+                      'assets/clock_layout/clock$index.png',
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                );
+              })),
         ),
       ),
     );
@@ -356,7 +360,7 @@ class _CountryState extends State<Country> {
                   key: ValueKey(i),
                   child: ListTile(
                     title:
-                        Text(Provider.of<Store>(context).countryListParsed[i]),
+                    Text(Provider.of<Store>(context).countryListParsed[i]),
                     onTap: () {
                       Provider.of<Store>(context, listen: false).setCountry(
                           Provider.of<Store>(context, listen: false)
@@ -365,16 +369,16 @@ class _CountryState extends State<Country> {
                           Provider.of<Store>(context, listen: false).country);
                       Provider.of<Store>(context, listen: false).index == -1
                           ? Provider.of<StoreTheme>(context, listen: false)
-                                  .country =
-                              Provider.of<Store>(context, listen: false)
-                                  .countryListParsed[i]
+                          .country =
+                      Provider.of<Store>(context, listen: false)
+                          .countryListParsed[i]
                           : Provider.of<Store>(context, listen: false)
-                                  .storedThemes[
-                                      Provider.of<Store>(context, listen: false)
-                                          .index]
-                                  .country =
-                              Provider.of<Store>(context, listen: false)
-                                  .countryListParsed[i]; //갱신된 지역 정보로 시간 또한 업데이트
+                          .storedThemes[
+                      Provider.of<Store>(context, listen: false)
+                          .index]
+                          .country =
+                      Provider.of<Store>(context, listen: false)
+                          .countryListParsed[i]; //갱신된 지역 정보로 시간 또한 업데이트
                     }, //StoreTheme에 있던 country정보를 Store에서 일괄 관리하는게 나을 것 같아서 이전하였습니다
                   ),
                 );
